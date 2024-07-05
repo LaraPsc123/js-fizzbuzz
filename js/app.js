1fd96b111ba0328109fc1d3818f9ac310b0c2f82
LaraPsc123 <larapascarella@gmail.com>
2024-07-05 11:31:05 +0200

    ADD: print in console number to 1 to 100

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('numeri da 1 a 100') // string
+
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+for (let i = 0; i < 100; i++) {
+    //console.log('number', i)
+    let number = i + 1 // number
+    console.log(number)
+}
+
