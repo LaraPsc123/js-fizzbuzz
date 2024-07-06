30b567218a8015b0f04ba0a82871a434351ef061
LaraPsc123 <larapascarella@gmail.com>
2024-07-06 14:32:49 +0200

    ADD: in list

--- js/app.js
+++ js/app.js
@@ -5,5 +5,16 @@ for (let i = 0; i < 100; i++) {
     //console.log('number', i)
     let number = i + 1 // number
     console.log(number)
+
+    // per i multipli di 3 stampi “Fizz” al posto del numero
+    if (number % 3)
+        console.log('Fizz')
+
+    //per i multipli di 5 stampi “Buzz”
+    if (number % 5)
+        console.log('Buzz')
 }
+
+
+
 
